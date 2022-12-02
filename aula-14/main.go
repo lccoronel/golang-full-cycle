a145065118eb4c368f201b06d164a6b05f61cfa6
Lucas Coronel <lucas.coronel@platformbuilders.io>
2022-12-02 09:17:09 -0300

    feat: ponteiros e structs

--- aula-14/main.go
+++ aula-14/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+type Cliente struct {
+	nome string
+}
+
+func (c *Cliente) andou() {
+	c.nome = "Lucas Coronel"
+	fmt.Printf("O cliente %s andou\n", c.nome)
+}
+
+func main() {
+	lucas := Cliente{
+		nome: "Lucas",
+	}
+
+	lucas.andou()
+	println(lucas.nome)
 }
