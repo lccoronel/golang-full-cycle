33484fb05356888acd73c1c2526bb262f55eb62b
Lucas Coronel <lucas.coronel@platformbuilders.io>
2022-12-02 08:07:54 -0300

    feat: ponteiros

--- ponteiros/main.go
+++ ponteiros/main.go
@@ -1,9 +1,18 @@
 package main
 
+func soma(a, b *int) int {
+	return *a + *b
+}
+
 func main() {
 	a := 10
 	var ponteiro *int = &a
 	*ponteiro = 20
 	b := &a
 	println(*b)
+
+	number1 := 2
+	number2 := 3
+
+	println(soma(&number1, &number2))
 }
