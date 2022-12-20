d85595d5d7baaf263d20616e318f485e822e4e1d
Lucas Coronel <lucas.coronel@platformbuilders.io>
2022-12-20 08:03:40 -0300

    feat: for

---
+++ aula-19/main.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+	// simples
+	// for i := 0; i < 10; i++ {
+	// 	println(i)
+	// }
+
+	// key e value
+	// numeros := []string{"um", "dois", "tres"}
+	// for k, v := range numeros {
+	// 	println(k, v)
+	// }
+
+	// while
+	// i := 0
+	// for i < 10 {
+	// 	println(i)
+	// 	i++
+	// }
+
+	// loop infinito
+	for {
+		print("loop")
+	}
+}
