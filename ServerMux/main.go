625254c6e089115d29ba2e38ce2185a5e5bcda5b
Lucas Coronel <lucas.coronel@platformbuilders.io>
2022-12-28 19:19:03 -0300

    feat: serve mux

---
+++ ServerMux/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", HomeHandler)
+	mux.Handle("/blog", blog{title: "my blog"})
+
+	http.ListenAndServe(":8080", mux)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world!"))
+}
+
+type blog struct {
+	title string
+}
+
+func (b blog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(b.title))
+}
