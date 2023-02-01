ded2ddb4eb064ce5d5298e2e649ea600a3095c1f
Lucas Coronel <lucas.coronel@platformbuilders.io>
2023-02-01 07:25:00 -0300

    feat: soft delete

--- database/gorm/main.go
+++ database/gorm/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,19 +9,33 @@ type Product struct {
 	ID    int `gorm:"primaryKey`
 	Name  string
 	Price float64
+	gorm.Model
 }
 
 func main() {
-	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 
-	db.AutoMigrate(&Product{})
+	database.AutoMigrate(&Product{})
 
+	// create
+	// database.Create(&Product{
+	// 	Name:  "Mackbook Pro",
+	// 	Price: 1000.00,
+	// })
+
+	// update
+	// var product Product
+	// database.First(&product, 1)
+	// product.Name = "iPad"
+	// database.Save(&product)
+
+	//delete
 	var product Product
-	db.First(&product, 2)
-	fmt.Println(product)
+	database.First(&product, 1)
+	database.Delete(&product)
 }
