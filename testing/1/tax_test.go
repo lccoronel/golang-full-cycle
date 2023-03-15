b6d34824ded5c66ec35493743f5f05a380996ba8
Lucas Coronel <lccoronel7@icloud.com>
2023-03-15 07:20:43 -0300

    feat: test batch

---
+++ testing/1/tax_test.go
@@ -0,0 +1,34 @@
+package tax
+
+import "testing"
+
+func TestCalculate(t *testing.T) {
+	amount := 500.0
+	expected := 5.0
+
+	result := CalculateTax(amount)
+
+	if result != expected {
+		t.Errorf("Expected %f but got %f", expected, result)
+	}
+}
+
+func TestCalculateBatch(t *testing.T) {
+	type calcTax struct {
+		amount, expect float64
+	}
+
+	table := []calcTax{
+		{500.5, 5.0},
+		{1000.5, 10.0},
+		{1500.5, 10.0},
+	}
+
+	for _, item := range table {
+		result := CalculateTax(item.amount)
+
+		if result != item.expect {
+			t.Errorf("Expected %f but got %f", item.expect, result)
+		}
+	}
+}
