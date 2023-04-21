00f4008cb814913b85a2a6169c533f3053afbc40
Lucas Coronel <lccoronel7@icloud.com>
2023-04-21 08:26:22 -0300

    feat: create product handler

--- apis/cmd/server/main.go
+++ apis/cmd/server/main.go
@@ -1,3 +1,63 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/lccoronel/golang-full-cycle/apis/configs"
+	"github.com/lccoronel/golang-full-cycle/apis/internal/dto"
+	"github.com/lccoronel/golang-full-cycle/apis/internal/entity"
+	"github.com/lccoronel/golang-full-cycle/apis/internal/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	http.ListenAndServe(":8000", nil)
+}
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (productHandler *ProductHandler) CreateProduct(response http.ResponseWriter, request *http.Request) {
+	var productDTO dto.CreateProductInput
+
+	err := json.NewDecoder(request.Body).Decode(&productDTO)
+	if err != nil {
+		response.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	product, err := entity.NewProduct(productDTO.Name, productDTO.Price)
+	if err != nil {
+		response.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = productHandler.ProductDB.Create(product)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	response.WriteHeader(http.StatusCreated)
+}
