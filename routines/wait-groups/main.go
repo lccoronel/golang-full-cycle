51a98b0da059a9afd0650d1eaf5c0fc56ac24f64
Lucas Coronel <lccoronel7@icloud.com>
2023-06-12 15:22:56 -0300

    feat: wait group

---
+++ routines/wait-groups/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func task(name string, wg *sync.WaitGroup) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d: Task %s is running\n", i, name)
+		time.Sleep(1 * time.Second)
+		wg.Done()
+	}
+}
+
+func main() {
+	waitGroup := sync.WaitGroup{}
+	waitGroup.Add(20)
+
+	go task("A", &waitGroup)
+	go task("B", &waitGroup)
+
+	waitGroup.Wait()
+}
